6a5d40f97b85820b7548aebdfea8db04d830f251
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2019-11-06 10:43:38 +0100

    feat(@commonly-buildtools/clean): create a new package

---
+++ packages/clean/index.ts
@@ -0,0 +1,19 @@
+import { spawnSync } from "child_process"
+import npmrunpath from "npm-run-path"
+
+
+
+spawnSync(
+    "tsc",
+    [
+        "--build",
+        "--clean",
+        ...process.argv.slice(2)
+    ],
+    {
+        cwd: process.cwd(),
+        shell: true,
+        stdio: "inherit",
+        env: npmrunpath.env()
+    }
+)
